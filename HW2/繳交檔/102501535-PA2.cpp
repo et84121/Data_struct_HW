c47d2c782eccd634abdcae9aaef399b5505b2344
et84121 <f411024@gmail.com>
2015-03-17 22:26:18 +0800

    作業繳交檔制作

---
+++ HW2/繳交檔/102501535-PA2.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <fstream> 
+#include <string>
+using namespace std;
+
+int count=0;
+
+//Permutation Generator
+//permutate a[k]~a[m]
+void PG(string a,const int k,const int m,ofstream *fout){   
+	if (k == m){
+		for (int i=0;i<=m;i++){
+			cout<<a[i]<<" ";
+			*fout<<a[i]<<" ";
+		}
+		cout<<endl;
+		*fout<<endl;
+		count++;//計數器+1 
+	}
+	else
+		for(int i=k;i<m;i++){
+			swap(a[k],a[i]);
+			PG(a,k+1,m,fout);
+			swap(a[k],a[i]);
+		}
+}
+
+int main (){
+    string file_order;	//要開啟的檔案序號 
+	string p;  //要排序的字串 
+	int length;
+	while(1){
+		string input_file_name="prog2-input-.txt";  //讀檔名 
+		string output_file_name="prog2-output-.txt"; //寫檔名 
+		cout<<"input the order of file"<<endl;
+		cin>>file_order;   
+		
+		input_file_name.insert(12, file_order);  //將 檔案序號 插入檔名中 
+		output_file_name.insert(13, file_order);
+		
+		ifstream fin (&input_file_name[0]);  //開檔 
+		ofstream fout(&output_file_name[0]);
+		
+		if(!fin) {cout<<"File read Error!!"<<endl;}  //檢查是否開檔成功 
+		if(!fout){cout<<"File write Error!!"<<endl;}
+		
+		
+		cout<<"Get string form file now"<<endl;
+		
+		while((fin>>length>>p)&&(length!=0)){  //讀檔並判斷是否到文未了 
+			cout<<p<<endl; 
+			count=0;//計數器歸零 
+			PG(p,0,p.size(),&fout);//進行排列運算
+			fout<<count<<endl;
+			cout<<count<<endl;
+		}
+	}
+	return 0;
+}
